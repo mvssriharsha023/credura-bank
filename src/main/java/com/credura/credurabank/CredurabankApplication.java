8a1228ad40cedeac4f21f5c13406f4d6e88b84c6
mvssriharsha023 <mvssriharsha9@gmail.com>
2026-01-03 13:10:01 +0530

    Connected to Database

---
+++ src/main/java/com/credura/credurabank/CredurabankApplication.java
@@ -0,0 +1,13 @@
+package com.credura.credurabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredurabankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CredurabankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/credura/credurabank/CredurabankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credura.credurabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredurabankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
